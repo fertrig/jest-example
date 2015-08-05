93f7a5c43dce89db2731ead50fc941d5fa8c7ab1
Fernando Trigoso <fertrig@gmail.com>
2015-08-05 16:42:28 -0400

    using official jest-cli and fixing test

--- __tests__/sub-test.js
+++ __tests__/sub-test.js
@@ -6,6 +6,6 @@ jest.dontMock('path');
 describe('subtract', function() {
     it('subs', function() {
         var sub = require('../src/subtract');
-        expect(sub(2,1)).toBe(5);
+        expect(sub(2,1)).toBe(1);
     });
 });
